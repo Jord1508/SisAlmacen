e317828e04ef0d70d743e0bd033f96d6365a946e
Pablo Brian <brianpari29@gmail.com>
2024-03-04 19:08:21 -0300

    crie un comentario en el main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
 from Vista.vLogin import VentanaLogin
-
+""" este es un comentario"""
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = VentanaLogin()
